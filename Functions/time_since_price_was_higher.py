2ac6cfbed8a7e487515559c94c9d91a0ad562929
tristanbob <tristan.rhodes@gmail.com>
2015-07-30 10:48:51 -0600

    Changed tabs into spaces

--- Functions/time_since_price_was_higher.py
+++ Functions/time_since_price_was_higher.py
@@ -1,9 +1,9 @@
 def time_since_price_was_higher(price_data):  
     # Create a series that only includes periods when the price was higher than now
     highers = price_data[price_data > price_data[-1]]
-	# Save the current datetime
+    # Save the current datetime
     now = price_data.index[-1]
-	# Save the datetime of the most recent high
+    # Save the datetime of the most recent high
     last_higher = highers.index[-1]
-	# Return a time delta since the previous high
+    # Return a time delta since the previous high
     return now - last_higher
