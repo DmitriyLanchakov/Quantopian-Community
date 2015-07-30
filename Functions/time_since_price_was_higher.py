e1a195670a5a01ca1b709d72faeafbe1674ea295
tristanbob <tristan.rhodes@gmail.com>
2015-07-30 10:47:17 -0600

    Added comments

--- Functions/time_since_price_was_higher.py
+++ Functions/time_since_price_was_higher.py
@@ -1,5 +1,9 @@
 def time_since_price_was_higher(price_data):  
-    highers = price_data[price_data > price_data[-1]]  
-    today = price_data.index[-1]  
-    last_higher = highers.index[-1]  
-    return today - last_higher  
+    # Create a series that only includes periods when the price was higher than now
+    highers = price_data[price_data > price_data[-1]]
+	# Save the current datetime
+    now = price_data.index[-1]
+	# Save the datetime of the most recent high
+    last_higher = highers.index[-1]
+	# Return a time delta since the previous high
+    return now - last_higher
--- Functions/time_since_price_was_lower.py
+++ Functions/time_since_price_was_lower.py
@@ -1,5 +1,9 @@
 def time_since_price_was_lower(price_data):  
+    # Create a series that only includes periods when the price was lower than now
     lowers = price_data[price_data < price_data[-1]]  
-    today = price_data.index[-1]  
-    last_lower = highers.index[-1]  
-    return today - last_lower  
+	# Save the current datetime
+    now = price_data.index[-1]  
+	# Save the datetime of the most recent low
+    last_lower = lowers.index[-1]  
+	# Return a time delta since the previous low
+    return now - last_lower
